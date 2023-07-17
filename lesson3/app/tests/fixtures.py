f020c5e5f989b571e8eb940a035bfbf28d760b45
Яков Михопаркин <ja.mix@mail.ru>
2023-07-17 11:15:37 +0300

    разделение фикстур

--- lesson3/app/tests/fixtures.py
+++ lesson3/app/tests/fixtures.py
@@ -1,11 +1,17 @@
 import pytest
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope='session')
 def prepare_db():
     """Подготовка базы для использования в тестах"""
     from app.main import app
     from app.main import init_mongo
     init_mongo(app, db_prefix="test_")
+
+
+@pytest.fixture(autouse=True, scope='function')
+def clean_db(prepare_db):
+    from app.main import app
     yield
     app.database['orders'].drop()
+
